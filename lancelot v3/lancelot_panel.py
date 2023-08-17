2578036def32f20e4a51f82d8dba2e200546b251
artutava <artutava@hotmail.com>
2023-08-17 19:22:31 -0300

    fix character properties panel
    fix character properties panel

--- lancelot v3/lancelot_panel.py
+++ lancelot v3/lancelot_panel.py
@@ -384,8 +384,8 @@ class LCT3_PT_Panel_Char_Options(Panel):
         layout = self.layout
         obj = context.object
         if bpy.context.object.type == 'ARMATURE':
-            row.label(text="Arms Offset")
             row = layout.row()
+            row.label(text="Arms Offset")
             row.prop(obj, '["Arm Offset"]',slider=True)
 
 
